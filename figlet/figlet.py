2abbed33bf56a4d6e114e6627fefa677fba6b93a
Aaron Thomas <84493696+arxhr007@users.noreply.github.com>
2023-01-11 17:36:13 +0530

    Create figlet.py

---
+++ figlet/figlet.py
@@ -0,0 +1,139 @@
+import os
+os.system('clear')
+r = "\033[31m"
+g = "\033[32m"
+y = "\033[33m"
+b = "\033[34m"
+p = "\033[35m"
+d = "\033[2;37m"
+w = "\033[0m"
+logo = f"""{g}
+      FIGLET MACHINE{r}
+      
+░░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄▄
+░░░░░█░░░░░░░░░░░░░░░░░░▀▀▄
+░░░░█░░░░░░░░░░░░░░░░░░░░░░█
+░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█
+░▄▀░▄▄▄░░█▀▀▀▀▄▄█░░░██▄▄█░░░░█
+█░░█░▄░▀▄▄▄▀░░░░░░░░█░░░░░░░░░█
+█░░█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄░█
+░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█
+░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█
+░░░█░░░░██░░▀█▄▄▄█▄▄█▄▄██▄░░█
+░░░░█░░░░▀▀▄░█░░░█░█▀█▀█▀██░█
+░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█
+░░░░░░░▀▄▄░░░░░░░░░░░░░░░░░░░█
+░░▐▌░█░░░░▀▀▄▄░░░░░░░░░░░░░░░█
+░░░█▐▌░░░░░░█░▀▄▄▄▄▄░░░░░░░░█
+░░███░░░░░▄▄█░▄▄░██▄▄▄▄▄▄▄▄▀
+░▐████░░▄▀█▀█▄▄▄▄▄█▀▄▀▄
+░░█░░▌░█░░░▀▄░█▀█░▄▀░░░█
+░░█░░▌░█░░█░░█░░░█░░█░░█
+░░█░░▀▀░░██░░█░░░█░░█░░█
+░░░▀▀▄▄▀▀░█░░░▀▄▀▀▀▀█░░█
+░░░░░░░░░░█░░░░▄░░▄██▄▄▀
+░░░░░░░░░░█░░░░▄░░████
+░░░░░░░░░░█▄░░▄▄▄░░▄█
+░░░░░░░░░░░█▀▀░▄░▀▀█
+░░░░░░░░░░░█░░░█░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█▄▄▄▐▄▄▄█
+░░░░░░░▄▄▄▄▀▄▄▀█▀▄▄▀▄▄▄▄
+░░░░░▄▀▄░▄░▄░░░█░░░▄░▄░▄▀▄
+░░░░░█▄▄▄▄▄▄▄▄▄▀▄▄▄▄▄▄▄▄▄█
+
+         {y}[FIGLET]
+ {b}Scripted by BLINKING-IDIOT{g}
+     insta:@_arxhr007_
+"""
+print(logo)
+a=["      A      ","     AAA     ","    A   A    ","   A     A   ","  AAAAAAAAA  "," A         A ","A           A"]
+b=["BBBBBBB","B     B","B     B","BBBBBB ","B     B","B     B","BBBBBBB"]
+c=["CCCCCCCC","C       ","C       ","C       ","C       ","C       ","CCCCCCCC"]
+d=["DDDDDDD ","D      D","D      D","D      D","D      D","D      D","DDDDDDD "]
+e=["EEEEEEEEEE","E         ","E         ","EEEEEEEEEE","E         ","E         ","EEEEEEEEEE"]
+f=["FFFFFFFFFF","F         ","F         ","FFFFFFFFFF","F         ","F         ","F         "]
+g=["GGGGGGGGG","G        ","G        ","G    GGGG","G       G","G       G","GGGGGGGGG"]
+h=["H       H","H       H","H       H","HHHHHHHHH","H       H","H       H","H       H"]
+i=["IIIIIIIII","    I    ","    I    ","    I    ","    I    ","    I    ","IIIIIIIII"]
+j=["JJJJJJJJJ","    J    ","    J    ","J   J    ","J   J    ","J   J    ","JJJJJ    "]
+k=["   K   K","   K  K ","   K K  ","   KK   ","   KK   ","   K K  ","   K  K "]
+l=["L        ","L        ","L        ","L        ","L        ","L        ","LLLLLLLLL"]
+m=["M          M","MM        MM","M M      M M","M  M    M  M","M   M  M   M","M    MM    M","M          M"]
+n=["N      N","NN     N","N N    N","N  N   N","N   N  N","N    N N","N     NN"]
+o=["OOOOOOOOOO","O        O","O        O","O        O","O        O","O        O","OOOOOOOOOO"]
+p=["PPPPPPPPPP","P        P","P        P","PPPPPPPPPP","P         ","P         ","P         "]
+q=["QQQQQQQQQQ ","Q        Q ","Q        Q ","Q      Q Q ","Q       QQ ","QQQQQQQQQQ ","          Q"]
+r=["RRRRRRRRRR","R        R","R        R","R        R","RRRRRRRRRR","RR        ","R RRRRR   "]
+s=["SSSSSSSSSS","S         ","S         ","SSSSSSSSSS","         S","         S","SSSSSSSSSS"]
+t=["TTTTTTTTTT","    T     ","    T     ","    T     ","    T     ","    T     ","    T     "]
+u=["U        U","U        U","U        U","U        U","U        U","U        U","UUUUUUUUUU"]
+v=["V           V"," V         V ","  V       V  ","   V     V   ","    V   V    ","     V V     ","      V      "]
+w=["W               W"," W             W ","  W     W     W  ","   W   W W   W   ","   W W   W W    ","     W     W     ","                 "]
+x=["X       X"," X     X ","  X   X  ","   XXX   ","  X   X  "," X     X ","X       X"]
+y=["Y       Y"," Y     Y ","  Y   Y  ","   Y Y   ","    Y    ","    Y    ","    Y    "]
+z=["ZZZZZZZ","     Z ","    Z  ","   Z   ","  Z    "," Z     ","ZZZZZZZ"]
+sp=["    ","    ","    ","    ","    ","    ","    "]
+te=input("Enter the word to figlet: ")
+te=te.lower()
+for j1 in range(7):
+ for i1 in range(len(te)):  
+  if te[i1]=='a':
+   print(a[j1]+" ",end="")
+  if te[i1]=='b':
+   print(b[j1]+" ",end="")
+  if te[i1]=='c':
+   print(c[j1]+" ",end="")
+  if te[i1]=='d':
+   print(d[j1]+" ",end="")
+  if te[i1]=='e':
+   print(e[j1]+" ",end="")
+  if te[i1]=='f':
+   print(f[j1]+" ",end="")
+  if te[i1]=='g':
+   print(g[j1]+" ",end="")
+  if te[i1]=="h":
+   print(h[j1]+" ",end="")
+  if te[i1]=='i':
+   print(i[j1]+" ",end="")
+  if te[i1]=='j':
+   print(j[j1]+" ",end="")
+  if te[i1]=='k':
+   print(k[j1]+" ",end="")
+  if te[i1]=='l':
+   print(l[j1]+" ",end="")
+  if te[i1]=='m':
+   print(m[j1]+" ",end="")
+  if te[i1]=='n':
+   print(n[j1]+" ",end="")
+  if te[i1]=='o':
+   print(o[j1]+" ",end="")
+  if te[i1]=='p':
+   print(p[j1]+" ",end="")
+  if te[i1]=='q':
+   print(q[j1]+" ",end="")
+  if te[i1]=='r':
+   print(r[j1]+" ",end="")
+  if te[i1]=='s':
+   print(s[j1]+" ",end="")
+  if te[i1]=='t':
+   print(t[j1]+" ",end="")
+  if te[i1]=='u':
+   print(u[j1]+" ",end="")
+  if te[i1]=='v':
+   print(v[j1]+" ",end="")
+  if te[i1]=='w':
+   print(w[j1]+" ",end="")
+  if te[i1]=='x':
+   print(x[j1]+" ",end="")
+  if te[i1]=='y':
+   print(y[j1]+" ",end="")
+  if te[i1]=='z':
+   print(z[j1]+" ",end="")
+ print()
+   
+ 
